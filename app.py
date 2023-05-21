b30ba859d6de571a71f5ececb4a02245b1f6e59b
Rivaldo Henry <123201279+Rivaldo747@users.noreply.github.com>
2023-05-21 17:38:53 -0500

    Add files via upload
    1st commit

---
+++ app.py
@@ -0,0 +1,111 @@
+import os
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import Response, JSONResponse
+import motor.motor_asyncio
+from bson import ObjectId
+import pydantic
+import requests
+from dotenv import load_dotenv
+from datetime import datetime
+load_dotenv()
+
+app= FastAPI()
+
+origins=[
+    "http://localhost:8000",
+    "https://simple-smart-hub-client.netlify.app"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+client= motor.motor_asyncio.AsyncIOMotorClient(os.getenv("mongo_db_url"))
+db= client.smat_control
+
+pydantic.json.ENCODERS_BY_TYPE[ObjectId]=str
+
+def getsunset_time():
+    response= requests.get("https://api.sunrise-sunset.org/json?lat=q7.7936&lng=-76.7936")
+    data= response.json()
+
+    sunset= data["results"]["sunset"]
+    sunset_time= datetime.datetime.strptime(sunset, "%I:%M:%S %p")
+    return sunset_time
+
+@app.get("/api/state")
+async def get_parameter():
+    temperature= await db["control_system"].find().sort("date").to_list(999)
+
+    """ fan= False"""
+    """ light= False"""
+
+    return temperature
+
+@app.put("/api/getparam", status_code=201)
+async def get_temperature(request:Request):
+    temp_object= await request.json()
+    
+    user_temp= await db["control_system"].insert_one(temp_object)
+    input_temp= await db["control_system"].find_one({"_id":user_temp.inserted_id})
+
+    return input_temp
+
+@app.get("/graph")
+async def get_parameter(request: Request):
+    n = int(request.query_params.get('size', 10))
+    sensor_input= await db["data_input"].find().to_list(n)
+    
+    presence= [param["presence"] for param in sensor_input]
+    temperatures=[param["temperature"] for param in sensor_input]
+    datetimes= [param["datetime"] for param in sensor_input]
+
+    output=[]
+    if temperatures and presence and datetimes:
+        output.append({
+            "temperature": temperatures,
+            "presence": presence,
+            "datetime": datetimes
+            })
+        
+    while len(output) < n:
+        output.append({
+            "temperature": 1.0,
+            "presence": False,
+            "datetime": datetime.now().time()
+        })
+
+    return output
+
+@app.put("/settings", status_code=201)
+async def create_parameter(request:Request):
+    parameter= await request.json()
+    global temp
+    temp= parameter["user_temp"]
+    global light
+    light= parameter["user_light"]
+    global duration_time
+    duration_time= parameter["light_duration"]
+
+    if light=="sunset":
+        global light_preference
+        light_preference=getsunset_time()
+    else: light_preference= datetime.strptime(light, "%H:%M:%S")
+
+    duration_time= light_preference+ parse_time (duration_time)
+
+    user_data={
+        "user_temp": temp,
+        "user_light": str(light_preference.time()),
+        "light_time_off":str(duration_time.time())
+    }
+    user_preference= await db["control_system"].insert_one(user_data)
+    input_preference= await db["control_system"].find_one({"_id":user_preference.inserted_id})
+
+    return input_preference
+    
+    
---
+++ readme.md
@@ -0,0 +1,36 @@
+To implement a IoT platform that controls basic appliances and allow user interaction through a webpage, 
+you will need to set up the hardware and develop the necessary software components. 
+How the setup of the project was carried out:
+
+Hardware Setup:
+
+Connect the Espressif ESP32 development module to the lamp and fan, ensuring proper electrical connections.
+Connect a digital temperature sensor  to the ESP32 module to measure the room temperature.
+Connect a passive infrared sensor to the ESP32 module to detect presence in the room.
+S
+oftware Setup:
+
+Install the required libraries for ESP32 development using Python.
+Set up the FastAPI Python framework to create the RESTful API. Install the necessary dependencies, such as FastAPI and Uvicorn, using pip.
+Create a new Python file to implement the API endpoints and functionality.
+
+Implementing the API:
+
+Define the necessary API routes and their corresponding HTTP methods in the Python file
+Implement the logic for each API endpoint based on the requirements mentioned.
+Use appropriate libraries or APIs to fetch the sunset time based on the user's location.
+Implement functions to control the lamps and fans connected to the ESP32 module.
+Implement functions to read data from the temperature sensor and passive infrared sensor.
+Implement the logic to combine the temperature-based control, time-based control, and presence detection features.
+Implement an API endpoint to retrieve the most recent sensor readings for visualization on the webpage.
+
+Webpage Integration:
+
+Host the webpage that allows users to interact with the IoT devices remotely. Ensure it has the necessary input fields and buttons to control the appliances and display sensor data.
+Make sure the webpage sends appropriate requests to your API endpoints based on user actions.
+
+
+Testing:
+when testing,
+Test the functionality of your IoT platform by interacting with the webpage and verifying that the appliances respond as expected.
+Test different scenarios, such as setting temperature and time-based controls, and observe the behavior of the appliances.
